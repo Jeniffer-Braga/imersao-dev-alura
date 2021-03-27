ef3999043569c37aa0f1603e52d773ae63550975
Jeniffer-Braga <jeniffercunhabraga@gmail.com>
2021-03-27 10:33:49 -0300

    Conversor de moedas

---
+++ main.js
@@ -0,0 +1,10 @@
+var valorEmDolar = parseFloat(prompt("Qual valor em dolar você deseja converter?"));
+
+var valorEmReal = valorEmDolar * 5.5;
+valorEmReal = valorEmReal.toFixed(2);
+
+alert(valorEmReal);
+
+
+
+
