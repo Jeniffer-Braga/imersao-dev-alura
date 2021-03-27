4e71e4205f146f622c10181916e158861bc824da
Jeniffer-Braga <jeniffercunhabraga@gmail.com>
2021-03-27 10:39:46 -0300

    Calculadora e condicionais

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
-var valorEmDolar = parseFloat(prompt("Qual valor em dolar você deseja converter?"));
+var operacao = prompt("Escolha a operação: 1 - Soma, 2 - Subtração, 3 - Multiplicação, 4 - Divisão");
+var operacaoString = "";
+var primeiroValor = parseInt(prompt("Digite o primeiro valor:"));
+var segundoValor = parseInt(prompt("Digite o segundo valor:"));
 
-var valorEmReal = valorEmDolar * 5.5;
-valorEmReal = valorEmReal.toFixed(2);
+if (operacao == 1) {
+  var resultado = primeiroValor + segundoValor;
+  operacaoString = " + ";
+} else if (operacao == 2) {
+  var resultado = primeiroValor - segundoValor;
+  operacaoString = " - ";
+} else if (operacao == 3) {
+  var resultado = primeiroValor * segundoValor;
+  operacaoString = " x ";
+} else if (operacao == 4) {
+  var resultado = primeiroValor / segundoValor;
+  operacaoString = " / ";
+} 
 
-alert(valorEmReal);
+console.log(primeiroValor + operacaoString + segundoValor + " = " + resultado);
+document.write("<h2>" + primeiroValor + operacaoString + segundoValor + " = " + resultado + "</h2>");  
+
+
+
 
 
 
