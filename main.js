44da5914416a37875c9851d8fcbe828bca5adc4b
Jeniffer-Braga <jeniffercunhabraga@gmail.com>
2021-03-30 16:04:39 -0300

    AluraFlix, interações e funções

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
-var listaFilmes = ["https://m.media-amazon.com/images/M/MV5BMTAxNDYxMjg0MjNeQTJeQWpwZ15BbWU3MDcyNTk2OTM@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BMjA5NDQyMjc2NF5BMl5BanBnXkFtZTcwMjg5ODcyMw@@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BMTkzMjgwMDg1M15BMl5BanBnXkFtZTcwMTgzNTI1OA@@._V1_UX182_CR0,0,182,268_AL_.jpg"];
-
-listaFilmes.push("https://m.media-amazon.com/images/M/MV5BNzMxNTExOTkyMF5BMl5BanBnXkFtZTcwMzEyNDc0OA@@._V1_UX182_CR0,0,182,268_AL_.jpg");
-
-for (var i = 0; i < listaFilmes.length; i++) {
-  document.write("<img class='movies' src=" + listaFilmes[i] + "/>");
+function adicionarFilme() {
+  var campoFilmeFavorito = document.querySelector('#filme')
+  var filmeFavorito = campoFilmeFavorito.value;
+  if (filmeFavorito.endsWith('.jpg')) {
+    listarFilmesNaTela(filmeFavorito);
+  } else {
+    alert("Imagem inválida.");
+  }
+  campoFilmeFavorito.value = "";
 }
 
-
+function listarFilmesNaTela(filme) {
+  var listaFilmes = document.querySelector('#listaFilmes');
+  var elementoFilme = "<img src=" + filme + " />";
+  listaFilmes.innerHTML = listaFilmes.innerHTML + elementoFilme;
+}
 
 
 
