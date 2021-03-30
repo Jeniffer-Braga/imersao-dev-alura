11a42c8a4e5d3a5e30912c4356eecdff9966e0da
Jeniffer-Braga <jeniffercunhabraga@gmail.com>
2021-03-30 20:48:11 -0300

    Super Trunfo e lógica do jogo

--- main.js
+++ main.js
@@ -1,60 +1,75 @@
-var jogador1 = {
-  nome: prompt("Qual o nome do primeiro jogador?"),
-  vitorias: 0,
-  empates: 0,
-  derrotas: 0,
-  pontos: 0 
-}
-var jogador2 = {
-  nome: prompt("Qual o nome do segundo jogador?"),
-  vitorias: 0,
-  empates: 0,
-  derrotas: 0,
-  pontos: 0
-}
-var jogadores = [jogador1, jogador2];
-
-jogador1.pontos = calculaPontos(jogador1);
-jogador2.pontos = calculaPontos(jogador2);
-
-function calculaPontos(jogador) {
-  var pontos = (jogador.vitorias * 3) + (jogador.empates);
-  return pontos; 
-}
-
-function exibirJogadores(jogadores) {
-  var html = "";
-  for (var i = 0; i < jogadores.length; i++) {
-    jogadores[i].pontos = calculaPontos(jogadores[i]);
-    
-    html += "<tr><td>" + jogadores[i].nome + "</td>";
-    html += "<td>" + jogadores[i].vitorias + "</td>";
-    html += "<td>" + jogadores[i].empates + "</td>";
-    html += "<td>" + jogadores[i].derrotas + "</td>";
-    html += "<td>" + jogadores[i].pontos + "</td>";
-    html += "<td><button onClick='adicionarVitoria(" + i + ")'>Vitória</button></td>";
-    html += "<td><button onClick='adicionarEmpate(" + i + ")'>Empate</button></td>";
-    html += "<td><button onClick='adicionarDerrota(" + i + ")'>Derrota</button></td></tr>";
+var carta0 = {
+  nome: "Seiya de pegaso",
+  atributos: {
+    ataque: 80,
+    defesa: 60,
+    magia: 90
   }
-  var tabelaJogadores = document.getElementById("tabelaJogadores");
-  tabelaJogadores.innerHTML = html;
 }
-exibirJogadores(jogadores);
+var carta1 = {
+  nome: "Bulbasaur",
+  atributos: {
+    ataque: 70,
+    defesa: 65,
+    magia: 85
+  }
+}
+var carta2 = {
+  nome: "Lorde Darth Vader",
+  atributos: {
+    ataque: 88,
+    defesa: 62,
+    magia: 90
+  }
+}
+var cartas = [carta0, carta1, carta2];
 
-function adicionarVitoria(i) {
-  var jogador = jogadores[i];
-  jogador.vitorias++;
-  exibirJogadores(jogadores);
+var cartaMaquina; 
+var cartaJogador;
+
+function sortearCarta() {
+var numeroCartaMaquina = parseInt(Math.random() * 3);
+cartaMaquina = cartas[numeroCartaMaquina];
+
+var numeroCartaJogador = parseInt(Math.random() * 3);
+while (numeroCartaJogador == numeroCartaMaquina){
+  numeroCartaJogador = parseInt(Math.random() * 3);
+}
+cartaJogador = cartas[numeroCartaJogador];
+console.log(cartaJogador);
+
+document.getElementById('btnSortear').disabled = true;
+document.getElementById('btnJogar').disabled = false;
+
+exibirOpcoes();
 }
 
-function adicionarEmpate(i) {
-  var jogador = jogadores[i];
-  jogador.empates++;
-  exibirJogadores(jogadores);
+function exibirOpcoes() {
+var opcoes = document.getElementById('opcoes');
+var valorOpcoes = "";
+for (var atributo in cartaJogador.atributos) { 
+  valorOpcoes += "<input type='radio' name='atributo' value='" + atributo + "'>" + atributo;  
+}
+opcoes.innerHTML = valorOpcoes;
 }
 
-function adicionarDerrota(i) {
-  var jogador = jogadores[i];
-  jogador.derrotas++;
-  exibirJogadores(jogadores);
+function pegarAtributoSelecionado() {
+var radioAtributo = document.getElementsByName('atributo');
+for (var i = 0; i < radioAtributo.length; i++) {
+  if (radioAtributo[i].checked) {
+    return radioAtributo[i].value;
+  }
+}
+}
+
+function jogar() {
+var atributoSelecionado = pegarAtributoSelecionado();
+if (cartaJogador.atributos[atributoSelecionado] > cartaMaquina.atributos[atributoSelecionado]) {
+  alert("Você venceu! Sua carta: " + cartaJogador.atributos[atributoSelecionado] + " Carta da máquina: " + cartaMaquina.atributos[atributoSelecionado] );
+} else if (cartaJogador.atributos[atributoSelecionado] < cartaMaquina.atributos[atributoSelecionado]) {
+  alert("Você perdeu! Sua carta: " + cartaJogador.atributos[atributoSelecionado] + " Carta da máquina: " + cartaMaquina.atributos[atributoSelecionado]);
+} else {
+  alert("Empate! Sua carta: " + cartaJogador.atributos[atributoSelecionado] + " Carta da máquina: " + cartaMaquina.atributos[atributoSelecionado])
+}
+console.log(cartaMaquina);
 }
